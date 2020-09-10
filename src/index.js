b4de29f88a1f9f0507ff55f779a5264c416f5f11
gsk567 <georgi.karagogov@gmail.com>
2020-09-11 00:07:05 +0300

    minor refactor

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import { useEmPageRouter } from "./router/protector";
 import initialStateMixin from "./state/initialStateMixin";
 
-export default {
-    useEmPageRouter,
-    initialStateMixin
-};
+export const useEmPageRouter = useEmPageRouter;
+export const initialStateMixin = initialStateMixin;
