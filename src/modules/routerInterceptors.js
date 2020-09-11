0e9b9157f3f9762a548ac407b75a7fc0ac468289
gsk567 <georgi.karagogov@gmail.com>
2020-09-11 18:25:24 +0300

    fix router interceptor

--- src/modules/routerInterceptors.js
+++ src/modules/routerInterceptors.js
@@ -5,23 +5,25 @@ const redirectToLogin = function(redirectUrl) {
 module.exports = function (router, store) {
     router.beforeEach((routeTo, routeFrom, next) => {
         if (typeof(fetch) !== 'undefined') {
-            fetch(routeTo.path, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json'
-                },
-                body: null,
-                credentials: 'include'
-            })
-                .then(response => response.json())
-                .then(responseData => {
-                    store.dispatch('updateData', responseData.data);
-                    next();
+            if (routeTo.path !== routeFrom.path) {
+                fetch(routeTo.path, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Accept': 'application/json'
+                    },
+                    body: null,
+                    credentials: 'include'
                 })
-                .catch(() => {
-                    redirectToLogin(routeTo.path);
-                });
+                    .then(response => response.json())
+                    .then(responseData => {
+                        store.dispatch('updateData', responseData.data);
+                        next();
+                    })
+                    .catch(() => {
+                        redirectToLogin(routeTo.path);
+                    });
+            }
         }
         else {
             if (process.env.VUE_ENV === 'server') {
